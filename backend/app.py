58a39643eb0a6e540fc80fb02a8d1cd20dbd0cdc
uzairname <23709618+uzairname@users.noreply.github.com>
2024-06-22 21:22:58 -0700

    save notifications to csv

--- backend/app.py
+++ backend/app.py
@@ -14,12 +14,36 @@ app = FastAPI()
 
 @app.post('/invoke')
 async def invoke():
-
     print('invoking model')
-
     # load data
     data = json.load(open('./data/submissions.json', 'r'))
-
     print(data)
     return 
+
+
+@app.post('/notify')
+async def notify(data: dict):
+
+    ngo_ids = data['ngo_ids']
+    report = data['report']
+
+    with open('./data/notifications.json', 'w') as f:
+        # update json file. 
+        # add new document for each ngo_id and report
+        for ngo_id in ngo_ids:
+            f.write(json.dumps({
+                'ngo_id': ngo_id,
+                'report': report
+            }))
+
+
+
+
+
+
+
+
+
+
+
 
--- backend/test.py
+++ backend/test.py
@@ -1,16 +1,36 @@
 import requests
 import asyncio
+import json
+import datetime
+
 
 async def test():
-
   # test post
-  result = requests.post('http://localhost:8000/invoke')
+  # result = requests.post('http://localhost:8000/invoke')
 
-  print(result.json())
+  # print(result.json())
+
+  ngo_ids = [
+    '1',
+    '2',
+    '3'
+  ]
+
+  report = 'This is a report'
+
+  with open('./data/notifications.csv', 'w') as f:
+    # update json file.
+    # add new document for each ngo_id and report
+    for ngo_id in ngo_ids:
+      f.write(','.join
+        
+        
+      #   {
+      #     'ngo_id': ngo_id,
+      #     'report': report,
+      #     'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      # }))
+
 
 if __name__ == '__main__':
   asyncio.run(test())
-
-
-
-
