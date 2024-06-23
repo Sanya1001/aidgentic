6a5beee1eda2bf9b05b58e2329769d7e106e6cc3
uzairname <23709618+uzairname@users.noreply.github.com>
2024-06-22 21:24:57 -0700

    notifications.csv

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,7 @@ import requests
 import asyncio
 import json
 import datetime
+import csv
 
 
 async def test():
@@ -18,13 +19,24 @@ async def test():
 
   report = 'This is a report'
 
-  with open('./data/notifications.csv', 'w') as f:
+  with open(r'./data/notifications.csv', 'a', newline='') as f:
     # update json file.
     # add new document for each ngo_id and report
+
+    fieldnames = ['ngo_id', 'timestamp', 'resources', 'report']
+
     for ngo_id in ngo_ids:
-      f.write(','.join
-        
-        
+      new_row = {
+        "ngo_id": ngo_id,
+        'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+        'resources': 'resources', 
+        'report': 'report'
+      }
+      
+      writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+      writer.writerow(new_row)
+
       #   {
       #     'ngo_id': ngo_id,
       #     'report': report,
